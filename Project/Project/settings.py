65a0f67f83742595ee5dbebe9cac2367b06b7931
WabukoWabuko <basilwabbs@gmail.com>
2025-10-21 11:43:07 +0000

    sETTING UP ALL THE LIBRARIES TO BE USED IN STORING IMAGES

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Modifications
+MEDIA_URL = '/media/' # URL to access media files
+MEDIA_ROOT = BASE_DIR / 'media' # Directory to store uploaded media files
